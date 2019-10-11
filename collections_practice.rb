30900ff429f002f69ed7ba9d85324b9bc96fe141
Ibrahim  Akar <ibsakar@gmail.com>
2019-10-11 16:38:54 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -34,8 +34,7 @@ def sum_array(array)
 end 
 
 def add_s(array)
- 
-      array.each_with_index.collect do |word,index|
+ array.each_with_index.collect do |word,index|
         if index != 1 
           word = word + "s"
         else
