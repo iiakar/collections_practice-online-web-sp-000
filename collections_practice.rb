e18e4df47dade443d2a0d473105590d751c460f8
Ibrahim  Akar <ibsakar@gmail.com>
2019-10-11 14:07:49 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,30 @@
+def sort_array_asc(array) 
+  array.sort {|x,y| x <=> y}
+end 
 
+def sort_array_desc(array)
+  array.sort {|x,y| y <=> x}
+end 
+
+def sort_array_char_count(array)
+  array = array.sort_by {|x| x.length}
+end 
+
+def swap_elements(array)
+  array[1],array[2] = array[2],array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end 
+
+def kesha_maker(array)
+  array.each do |item|
+    item[2] = "$"
+  end 
+end 
+
+def find_a(array)
+  array.find {|b| return b.start_with?(a) }
+end 
